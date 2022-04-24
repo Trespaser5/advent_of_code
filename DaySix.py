28d198c19d8b335bcad17a7716ec31783d1c3ebe
kyle <kyletansill@gmail.com>
2022-04-24 11:28:33 +0100

    Trying to solve the 256 day problem using nested threading. Currently having some issues

--- DaySix.py
+++ DaySix.py
@@ -1,22 +1,26 @@
-import enum
+import enum, threading
 
 
 class FishEventIndex(enum.Enum):
     BREED = 0
     RESET_TIME = 6
-    NEW_FISH_TIMER = 7
+    NEW_FISH_TIMER = 8
 
 
 class LanternFish(object):
 
     _internal_timer = 0
     _egg = False
+    _just_hatched = False
 
-    def __init__(self, initial_value, egg=False):
+    def __init__(self, initial_value, egg=False, just_hatched=False):
 
         self._egg = egg
-
         self._internal_timer = initial_value
+        self._just_hatched = just_hatched
+
+    def __repr__(self):
+        return str(self._internal_timer)
 
     def get_timer(self):
 
@@ -27,9 +31,15 @@ class LanternFish(object):
 
     def age(self):
 
-        new_time = self._internal_timer -1
+        if self._just_hatched:
+            # reset just hatched state but do not age
+            return self.__class__(self._internal_timer, self._egg)
 
-        return self.__class__(new_time, self._egg)._check_for_event(new_time)
+        else:
+
+            new_time = self._internal_timer -1
+
+            return self.__class__(new_time, self._egg)._check_for_event(new_time)
 
     def _check_for_event(self, time_value):
 
@@ -48,10 +58,12 @@ class LanternFish(object):
 class Shoal(object):
 
     _fish = []
+    _fish_class = None
 
     def __init__(self, fish, create=None, fish_class=LanternFish):
 
         self._fish = fish
+        self._fish_class = fish_class
 
         if create is not None:
             for start_time in create:
@@ -59,6 +71,119 @@ class Shoal(object):
 
     def get_fish(self):
         return self._fish
+
+    def get_size(self):
+        return len(self._fish)
+
+    def hatch(self):
+
+        shoal = self._fish
+
+        for i, fish in enumerate(shoal):
+            if fish.has_egg():
+                shoal[i] = self._fish_class(FishEventIndex.RESET_TIME.value, just_hatched=True)
+                shoal.append(self._fish_class(FishEventIndex.NEW_FISH_TIMER.value, just_hatched=True))
+
+        return Shoal(shoal)
+
+    def pass_days(self, days):
+
+        new_shoal = self
+
+        for day in range(0, days+1):
+            if day > 0:
+                new_shoal = new_shoal.hatch()
+                new_shoal = Shoal(fish=[fish.age() for fish in new_shoal.get_fish()])
+
+        return new_shoal
+
+    def pass_one_day(self):
+
+        new_shoal = self
+
+        new_shoal = new_shoal.hatch()
+        new_shoal = Shoal(fish=[fish.age() for fish in new_shoal.get_fish()])
+
+        return new_shoal
+
+
+class ShoalProcessing:
+
+    _shoals = {}
+    _threads = {}
+    _temp_shoals = []
+
+    def __init__(self, shoals=None, threads=None):
+
+        if shoals is None:
+            self._shoals = {}
+        else:
+            self._shoals = shoals
+
+        if threads is None:
+            self._threads = {}
+        else:
+            self._threads = threads
+
+    def process_big(self, init_shoal, days_to_run):
+
+        #enter try to handle empty array
+        try:
+            ci = [*self._shoals.keys()][-1:][0] + 1
+        except IndexError:
+            ci = 0
+
+        if isinstance(init_shoal, list):
+            self._shoals[ci]=(Shoal([], create=init_shoal))
+        elif isinstance(init_shoal, Shoal):
+            self._shoals[ci] = (init_shoal)
+        else:
+            raise ValueError("Shoal must of type list or Shoal")
+
+        current_day = 0
+        for day in range(0, days_to_run):
+
+            self._shoals[ci] = self._shoals[ci].pass_one_day()
+            print("day {}".format(day))
+
+            if self._shoals[ci].get_size() > 600:
+                current_day = day
+                break
+
+        days_left = days_to_run - current_day
+
+        for chunk in [self._shoals[ci].get_fish()[i:i+200] for i in range(0, self._shoals[ci].get_size(), 200)]:
+
+            try:
+                ti = [*self._shoals.keys()][-1:][0] + 1
+            except IndexError:
+                ti = 0
+
+            self._threads[ti] = \
+                threading.Thread(target=self.process_big, args=([Shoal(chunk), days_left]))
+            self._threads[ti].start()
+            self._threads[ti].join()
+
+    @staticmethod
+    def return_shoal(shoals):
+
+        return [fish for shoal in shoals.values() for fish in shoal.get_fish()]
+
+    def shoal_size(self):
+
+        size = 0
+
+        for shoal in self._shoals.values():
+            size += len(shoal.get_fish())
+
+        return size
+
+
+
+
+
+
+
 
 
 
--- DaySixTest.py
+++ DaySixTest.py
@@ -1,5 +1,6 @@
 import unittest
-from DaySix import LanternFish, Shoal
+from DaySix import LanternFish, Shoal, ShoalProcessing
+import threading
 
 
 class LanternFishTest(unittest.TestCase):
@@ -13,7 +14,7 @@ class LanternFishTest(unittest.TestCase):
 
         self.assertEqual(lantern_fish.get_timer(), 1)
 
-    def test_ay_egg(self):
+    def test_lay_egg(self):
 
         fish = LanternFish(3).lay_egg()
 
@@ -28,8 +29,74 @@ class ShoalTest(unittest.TestCase):
 
         self.assertEqual(shoal.get_fish()[1].get_timer(), 7)
 
+    def test_hatch(self):
+
+        shoal = Shoal([LanternFish(3, egg=True)])
+
+        shoal = shoal.hatch()
+
+        self.assertEqual(len(shoal.get_fish()), 2)
+
+    def test_pass_days(self):
+
+        shoal = Shoal([LanternFish(3)])
+
+        self.assertEqual(len(shoal.pass_days(5).get_fish()), 2)
+
+    def test_day_6_task_1(self):
+
+        shoal = Shoal([], create=[3,4,3,1,2])
+
+        shoal1 = shoal.pass_days(18)
+        shoal2 = shoal.pass_days(80)
+
+        self.assertEqual(len(shoal1.get_fish()), 26)
+        self.assertEqual(len(shoal2.get_fish()), 5934)
+
+    def test_day_6_task_1_sp(self):
+
+        init_shoal = [3,4,3,1,2]
+
+        sp1 = ShoalProcessing()
+        sp1.process_big(init_shoal, 18)
+
+        self.assertEqual(sp1.shoal_size(), 26)
+
+        sp2 = ShoalProcessing()
+        sp2.process_big(init_shoal, 80)
+
+        self.assertEqual(sp2.shoal_size(), 5934)
+
+    def test_task_1(self):
+
+        with open("d6t1.txt") as f:
+            existing_shoal = [int(character) for character in f.read() if character.isdigit()]
+
+        sp1 = ShoalProcessing()
+        sp1.process_big(existing_shoal, 80)
+
+        self.assertEqual(sp1.shoal_size(), 374927)
+    """
+    def test_task_2(self):
+
+        with open("d6t1.txt") as f:
+            existing_shoal = [int(character) for character in f.read() if character.isdigit()]
+
+        sp1 = ShoalProcessing()
+        sp1.process_big(existing_shoal, LanternFish, threading.Thread, 256)
+
+        self.assertEqual(sp1.shoal_size(), 374927)
+        """
 
 
+"""
+    def test_task_2(self):
+
+        with open("d6t1.txt") as f:
+            existing_shoal = [int(character) for character in f.read() if character.isdigit()]
+
+        self.assertEqual(len(Shoal([], create=existing_shoal).pass_days(256).get_fish()), 374927)
+    """
 
 
 
